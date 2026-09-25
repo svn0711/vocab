6fa042b2885818d443bb749b79fb2a1d8748db1f
svn0711 <oh.sebun.fm@gmail.com>
2026-09-25 09:12:39 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+const C = "vocab-v1";
+const FILES = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(C).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
+self.addEventListener("activate", e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== C).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener("fetch", e => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(
+    fetch(e.request)
+      .then(res => { const copy = res.clone(); caches.open(C).then(c => c.put(e.request, copy)); return res; })
+      .catch(() => caches.match(e.request))
+  );
+});
